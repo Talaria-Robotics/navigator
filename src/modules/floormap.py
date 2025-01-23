f7ba99c705f94ee14a2260ef49d7bdfa399e4ba1
Yoshi Askharoun <jjask7@gmail.com>
2025-01-23 16:21:10 -0500

    [WIP] SVG path parsing

--- src/modules/floormap.py
+++ src/modules/floormap.py
@@ -1,5 +1,18 @@
+from svgpathtools import parse_path
+
 class FloorMap:
-    def parse(filePath: str) -> None:
-        with open(filePath, "rb") as file:
-            file.buffer.read
-            pass
+    def parse(self, filePath: str) -> None:
+        floormap_lines = []
+        with open(filePath, "r") as file:
+            floormap_lines = file.readlines()
+        
+        paths = []
+        for line in floormap_lines:
+            path = parse_path(line)
+            paths.append(path)
+            print(path)
+
+
+if __name__ == "__main__":
+    floormap = FloorMap()
+    floormap.parse(r".\maps\TestA.floormap")
