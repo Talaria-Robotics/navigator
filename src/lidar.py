e1a4840ec1e850a93cd30bc4de6195522a82255f
Yoshi Askharoun <jjask7@gmail.com>
2025-02-13 11:12:20 -0600

    Initial LIDAR module

--- src/lidar.py
+++ src/lidar.py
@@ -1,26 +1,37 @@
-from math import floor
 from adafruit_rplidar import RPLidar
+from math import floor
+from sys import float_info
 
 PORT_NAME = '/dev/ttyUSB0'
 lidar = RPLidar(None, PORT_NAME, timeout=3)
 
-# used to scale data to fit on the screen
-max_distance = 0
+scan_data = [0] * 360
 
-def process_data(data):
-    print(data)
+def testPrint(data):
+    angles_of_interest = [0, 90, 180, 270]
+    for angle in angles_of_interest:
+        dist = data[angle]
+        print(f"{angle}°: {dist}")
+    
+    nearest_angle = 0
+    nearest_dist = float_info.max
+    for angle, dist in zip(range(len(data)), data):
+        if dist < nearest_dist:
+            nearest_dist = dist
+            nearest_angle = angle
+    print(f"Nearest: {nearest_angle}°, {nearest_dist}")
+    print()
 
-scan_data = [0]*360
-
-try:
-    #print(lidar.get_info())
+def scan() -> list[float]:
+    lidar.clear_input()
     for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-        process_data(scan_data)
+        for (quality, angle, distance) in scan:
+            clampedAngle = min([359, round(angle)])
+            scan_data[clampedAngle] = distance
+    
+        # Return early, we just care about the current iteration
+        return scan_data
 
-except KeyboardInterrupt:
-    print('Stopping.')
-
-lidar.stop()
-lidar.disconnect()
+def disconnect():
+    lidar.stop()
+    lidar.disconnect()
