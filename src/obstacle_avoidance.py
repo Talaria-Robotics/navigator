0ef12e77ed03ca8ff56b97dcfe87b8ccaa5d8480
Bianca <biancayoulton@gmail.com>
2025-04-14 01:57:54 -0500

    Box List Code Added

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -5,6 +5,27 @@ import lidar as ld
 
 scan_data = ld()
 
+# Creating the "Box of View"
+box = []
+for x in range(0,58):
+    value = 15/math.cos(x)
+    box.append(value)
+for x in range(59,89):
+    value = 24/(math.cos(90-x))
+    box.append(value)
+
+box.append(24)
+
+for x in range(91,147):
+    value = 24/(math.cos(x-90))
+    box.append(value)
+for x in range(148,180):
+    value = 15/(math.cas(180-x))
+    box.append(value)
+
+print("List of Distances inside Box: ", box)
+
+"""
 # Can adjust the target zone as desired
 focus_zone_right = scan_data[0:90]
 focus_zone_left = scan_data[269:359]
@@ -55,3 +76,4 @@ if(((r_counter > 10) or (l_counter > 10)) and ((r_obstacle == 1) or (l_obstacle 
         l_obstacle = 1
     
     # Will need to move accordingly... TBD...
+"""
