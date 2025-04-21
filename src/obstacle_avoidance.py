576a14b21a5cf5832a49772844b87fa39f763cb7
Yoshi Askharoun <jjask7@gmail.com>
2025-04-21 13:15:42 -0500

    Package up obstacle_avoidance moduke

--- src/obstacle_avoidance.py
+++ src/obstacle_avoidance.py
@@ -1,79 +1,52 @@
-import math
-import time
-import lidar as ld
+import numpy as np
+from typing import Union
 
+def _cosDeg(thetaDeg: float) -> float:
+    thetaRad = np.deg2rad(thetaDeg)
+    return float(np.cos(thetaRad))
 
-scan_data = ld()
+def getMinimumDistances() -> list[float]:
+    # Creating the "Box of View"
+    box = []
+    for x in range(0, 59):
+        value = 15 / _cosDeg(x)
+        box.append(value)
+    for x in range(59, 90):
+        value = 24 / (_cosDeg(90-x))
+        box.append(value)
 
-# Creating the "Box of View"
-box = []
-for x in range(0,58):
-    value = 15/math.cos(x)
-    box.append(value)
-for x in range(59,89):
-    value = 24/(math.cos(90-x))
-    box.append(value)
+    box.append(24)
 
-box.append(24)
-
-for x in range(91,147):
-    value = 24/(math.cos(x-90))
-    box.append(value)
-for x in range(148,180):
-    value = 15/(math.cas(180-x))
-    box.append(value)
-
-print("List of Distances inside Box: ", box)
-
-"""
-# Can adjust the target zone as desired
-focus_zone_right = scan_data[0:90]
-focus_zone_left = scan_data[269:359]
-
-# Initializing counters for how many points have obstacles
-r_counter = 0
-l_counter  = 0
-
-# Initializing counters for an obstacle detected
-r_obstacle = 0
-l_obstacle = 0
-
-# -------------------------------------------------------------------------------------------------------------------
-# Check the distances on the right side if there is an obstacle -------------> MAY NEED TO ADJUST THE OBSTACLE RANGE!
-for x in range(len(focus_zone_right)):
+    for x in range(91, 148):
+        value = 24 / (_cosDeg(x-90))
+        box.append(value)
+    for x in range(148, 180):
+        value = 15 / (_cosDeg(180-x))
+        box.append(value)
     
-    # Check for obstacles on RIGHT side
-    if((focus_zone_right[x] >= 10) or (focus_zone_right[x] <= 500)):
-        r_counter += 1
+    return box
 
-    # Check for obstacles on LEFT side
-    if((focus_zone_left[x] >= 10) or (focus_zone_left[x] <= 500)):
-        l_counter += 1
+def nearestWithinBox(scanData: list[float]) -> Union[tuple[int, float], None]:
+    """
+    Returns the angle and distance of the nearest obstacle within
+    the box window, or None if no obstacle was within the box.
+    """
+    minDistances = getMinimumDistances()
+    for angle in range(0, 180):
+        measured = scanData[angle]
+        minimum = minDistances[angle]
+        if measured <= minimum:
+            return angle, measured
+    return None
 
-# -------------------------------------------------------------------------------------------------------------------
-# There is a DYNAMIC obstacle
-if(((r_counter > 10) or (l_counter > 10)) and ((r_obstacle == 0) or (l_obstacle == 0))):
-    if((r_counter > 10)):
-        print("Obstacle on RIGHT side!")
-        r_obstacle = 1
 
-    if((l_counter > 10)):
-        print("Obstacle on LEFT side!")
-        l_obstacle = 1
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    r = getMinimumDistances()
+    theta = list(range(0, 180))
+    for a in theta:
+        theta[a] = np.deg2rad(a)
     
-    # Wait to see if the obstacle moves (DYNAMIC OBSTACLE)
-    time.sleep(10)
-
-# -------------------------------------------------------------------------------------------------------------------
-# There is a STATIC obstacle
-if(((r_counter > 10) or (l_counter > 10)) and ((r_obstacle == 1) or (l_obstacle == 1))):
-    if((r_counter > 10)):
-        print("Obstacle on RIGHT side!")
-        r_obstacle = 1
-
-    if((l_counter > 10)):
-        print("Obstacle on LEFT side!")
-        l_obstacle = 1
-    
-    # Will need to move accordingly... TBD...
-"""
+    plt.polar(theta, r)
+    plt.show()
